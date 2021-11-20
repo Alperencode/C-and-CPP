975a49c93dde14cb6365c6366b62ffb27daf0a18
Alperen Aga <alperencode@gmail.com>
2021-11-20 20:20:53 +0300

    Update all files

---
+++ KapananYildiz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	int j,k;
+	
+	for(j = 0; j<=14; j++){
+		
+		if(j<7){
+			for(k = 0; k <= j; k++){
+				printf("*");
+			}
+			printf("\n");
+		}else{
+			for(k=0; k<=14-j; k++){
+				printf("*");
+			}
+			printf("\n");
+		}
+	}
+}
