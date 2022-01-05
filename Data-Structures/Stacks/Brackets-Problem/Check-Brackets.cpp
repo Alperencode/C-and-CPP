c15a0aef52b62b49e8713504b4e78b5d981dd3d6
Alperen Aga <alperencode@gmail.com>
2022-01-05 03:53:06 +0300

    Reorganize Data-Structures/Stacks

--- Data-Structures/Stacks/Brackets-Problem/Check-Brackets.cpp
+++ Data-Structures/Stacks/Brackets-Problem/Check-Brackets.cpp
@@ -26,7 +26,7 @@ int main() {
     std::string text;
     getline(std::cin, text);
 
-	std::stack <Bracket> opening_brackets_stack;
+    std::stack <Bracket> opening_brackets_stack;
     int flag = 0;
     int position;
     for (position = 0; position < text.length(); ++position) {
@@ -34,32 +34,32 @@ int main() {
 
         if (next == '(' || next == '[' || next == '{') {
             // Process opening bracket, write your code here
-    		opening_brackets_stack.push(Bracket(next,position));        
+            opening_brackets_stack.push(Bracket(next,position));        
         }
 
         if (next == ')' || next == ']' || next == '}') {
-        	// Process closing bracket, write your code here
-        	if(opening_brackets_stack.empty()){
-				flag = 1;
-				break;
-			}else{
-				if(opening_brackets_stack.top().Matchc(next))
-					opening_brackets_stack.pop();
-				else{
-					opening_brackets_stack.push(Bracket(next,position));
-					flag = 1;
-					break;
-				}
-			}
+            // Process closing bracket, write your code here
+            if(opening_brackets_stack.empty()){
+                flag = 1;
+                break;
+            }else{
+                if(opening_brackets_stack.top().Matchc(next))
+                    opening_brackets_stack.pop();
+                else{
+                    opening_brackets_stack.push(Bracket(next,position));
+                    flag = 1;
+                    break;
+                }
+            }
         }
     }
 
     // Printing answer, write your code here
     if(opening_brackets_stack.empty() && flag == 0)
-    	std::cout << "Success";
+        std::cout << "Success";
     else{
-    	std::cout << opening_brackets_stack.top().position+1;
-	}
+        std::cout << opening_brackets_stack.top().position+1;
+    }
 
     return 0;
 }
