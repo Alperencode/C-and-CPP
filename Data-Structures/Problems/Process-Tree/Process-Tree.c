0ebadda68e2e21b452e0baffd6b6ce374402edd5
Alperen Aga <alperencode@gmail.com>
2023-03-29 17:53:45 +0300

    Update Problems/Process-Tree Add Parent process section (sum of leaf nodes) Add Makefile

--- Data-Structures/Problems/Process-Tree/Process-Tree.c
+++ Data-Structures/Problems/Process-Tree/Process-Tree.c
@@ -8,11 +8,34 @@ void createTree(int height){
 
     if(height>0){
         pid_t pid = fork();
+
         if(pid>0){
             pid_t childPid = fork();
 
             if(childPid != 0){
                 // Parent process
+                wait(NULL);
+                wait(NULL);
+
+                char processFile1[20], processFile2[20], parentFile[20];
+                sprintf(processFile1, "%d.txt", pid);
+                sprintf(processFile2, "%d.txt", childPid);
+                sprintf(parentFile, "%d.txt", getpid());
+
+                FILE *file1 = fopen(processFile1, "r");
+                FILE *file2 = fopen(processFile2, "r");
+
+                int num1, num2;
+                fscanf(file1, "%d", &num1);
+                fscanf(file2, "%d", &num2);
+                int sum = num1 + num2;
+
+                FILE *fp = fopen(parentFile, "w");
+                fprintf(fp, "Sum of (%d and %d): %d\n", num1, num2, sum);
+
+                fclose(fp);
+                fclose(file1);
+                fclose(file2);
             }else{
                 // Right child
                 createTree(--height);
@@ -26,12 +49,12 @@ void createTree(int height){
         srand(getpid());
         int random = (rand() % 101);
 
-        char fileName[20];
-        sprintf(fileName, "%d.txt", getpid());
-        FILE *fp = fopen(fileName, "w");
+        char leafNode[20];
+        sprintf(leafNode, "%d.txt", getpid());
+        FILE *leafFile = fopen(leafNode, "w");
 
-        fprintf(fp, "%d\n", random);
-        fclose(fp);
+        fprintf(leafFile, "%d\n", random);
+        fclose(leafFile);
     }
 
 }
